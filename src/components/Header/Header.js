d933bf8e8b371596950f57d456f60c78ff371b8e
NatalliaSaverchenko <saverchenko.nn@gmail.com>
2021-09-10 01:31:22 +0300

    add header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,43 @@
+import { Link, useLocation } from 'react-router-dom';
+import { Button } from 'antd';
+import { setIsLoggedIn,setUser } from '../../redux/actions/userActions';
+import logo from '../../image/sibdev-logo.png';
+
+import './header.css';
+import { useDispatch } from 'react-redux';
+
 const Header=()=>{
-  return (<div><h1>Header</h1></div>);
+  const reduxDispatch=useDispatch();
+  let pathName = useLocation().pathname;
+  const handelClick=()=>{
+    localStorage.removeItem('authToken');
+    reduxDispatch(setIsLoggedIn(false));
+    reduxDispatch(setUser(''));
+  };
+  return (<div>
+
+    <header className='header'>
+      <div className='head__group'>
+        <img
+          className='header__logo'
+          src={logo}
+          alt='logo'/>
+        <Link
+          className={'header__item-search' +
+            (pathName === '/' ? ' header__item-search_active' : '')}
+          to='/'>Поиск</Link>
+        <Link
+          className={'header__item-favorites' +
+            (pathName === '/favorites' ? ' header__item-favorites_active' : '')}
+          to='/favorites'>Избранное</Link>
+      </div>
+      <div className='head__group'>
+        <Button
+          className='header__item-exit'
+          variant='outline-primary'
+          onClick={handelClick}>Выйти</Button>
+      </div>
+    </header>
+  </div>);
 };
 export default Header;
